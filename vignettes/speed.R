f54d19e1c329eb2af9bfa05c1f2d99567b0247fd
Bailey <pbailey@air.org>
2016-03-04 16:16:58 -0500

    as cran check passes better vignette data

--- vignettes/speed.R
+++ vignettes/speed.R
@@ -4,8 +4,8 @@ require(wCorr)
 
 grid <- expand.grid(fast=c(TRUE,FALSE),
                     ML=c(TRUE,FALSE),
-                    iter=3,
-                    n = round(10^seq(1,4,by=0.25)),
+                    iter=20,
+                    n = round(10^seq(1,5,by=0.25)),
                     rho = c(-0.99,seq(-0.95,0.95,by=0.05), 0.99))
 grid$reset <- (grid$ML) & (grid$fast)
 speed <- wCorrSim(n=grid$n, rho=grid$rho, ML=grid$ML, fast=grid$fast, reset=grid$reset, usew=FALSE)
