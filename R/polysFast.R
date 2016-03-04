dc4e927712e2130397b990fdde3cd49ae28d6aee
Ahmad Emad <ahmademad@gmail.com>
2016-03-04 12:41:46 -0500

    FFix polyc

--- R/polysFast.R
+++ R/polysFast.R
@@ -1,15 +1,32 @@
 #' @importFrom minqa bobyqa
 polysFast <- function(x, M, w, ML=FALSE) {
+  M <- as.numeric(as.factor(M))
+  uM <- sort(unique(M))
+  mapTheta <- function(v) {
+    vv <- cumsum(c(v[1],exp(v[-1])))
+    c(NA,-Inf,vv,Inf)
+  }
+  theta0 <- sapply(uM[-length(uM)],function(z) qnorm(weighted.mean(M<=z, w)) )
   if(ML) {
-    values = mainF(x, M, w)
+    #values = mainF(x, M, w, theta0)
+    #theta0 <- imapThetaFast2(theta(M));
+    #bob <- bobyqa(par=c(imapCor(cor(x,M)),imapTheta(theta0)), fn=optF(x,M,w))
+    temp = w/sum(w)
+    temp2 = fixxFast(x, temp)
+    temp3 = sum(temp*dnorm(temp2,log=TRUE))
     bob <- bobyqa(par=c(atanh(cor(x,M)),imapThetaFast2(theta0)),
-                  fn=optFcFast, x=unlist(values[5]),  w=w, temp1=unlist(values[2]),temp2=unlist(values[3]),
-                                temp3=unlist(values[4]))
+                  fn=optFFast, x=temp2,  w=temp, M=M, temp3 = temp3)
     return(  tanh(bob$par[1]))
   } else {
-    values = mainF(x, M, w)
+    temp = w/sum(w)
+    temp2 = fixxFast(x, temp)
+    temp3 = sum(temp*dnorm(temp2,log=TRUE))
+
+    values = mainF(x, M, w, theta0)
      #opt <- optimize(mainF, interval = imapCorFast(cor(x,M)) + c(-3,3), x,w,temp1, temp2, temp3)
-    opt <- optimize(optFcFast, interval=unlist(values[1]), unlist(values[5]), w, unlist(values[2]),unlist(values[3]), unlist(values[4]))
+    opt <- optimize(optFcFast, interval=unlist(values[1]),
+                    x=temp2, w=temp, theta0=(imapThetaFast2(theta0)),
+                    M=M, temp3= temp3)
     return( tanh(opt$minimum) )
   }
 }
