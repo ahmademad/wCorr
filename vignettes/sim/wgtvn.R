0c9fb0e6b716b1f5fb539fc3578705a52fb05bb1
Bailey <pbailey@air.org>
2016-04-18 09:50:43 -0400

    more simulation updates fixing Arguments add captioner (used in vignette)

--- vignettes/sim/wgtvn.R
+++ vignettes/sim/wgtvn.R
@@ -5,8 +5,8 @@ source("sim.R")
 
 grid <- expand.grid(usew=c(FALSE,TRUE),
                     iter=1:2,
-                    n = c(10,100,1000,10000,100000),
-                    rho = c(-0.99,seq(-0.95,0.95,by=0.05), 0.99))
+                    n = c(10,100,1000,10000),
+                    rho = c(-0.99,seq(-0.95,0,by=0.05)))
 grid$reset <- !grid$usew
 
 wgtvn <- wCorrSim(n=grid$n, rho=grid$rho, ML=FALSE, fast=TRUE, reset=TRUE, usew=grid$usew)
