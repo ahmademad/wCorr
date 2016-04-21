3a6af7e1931e7f5176a063f3013d36fe9a5b1acd
Bailey <pbailey@air.org>
2016-04-21 01:43:48 -0400

    optimize sim.R a bit with sim2.R add wgtvn.RData

--- vignettes/sim/speed.R
+++ vignettes/sim/speed.R
@@ -11,7 +11,7 @@ grid1 <- expand.grid(fast=c(TRUE,FALSE),
 grid2 <- expand.grid(fast=c(TRUE,FALSE),
                      ML=c(TRUE,FALSE),
                      iter=20,
-                     n = round(10^seq(5,7,by=0.25)),
+                     n = round(10^seq(5,6,by=0.25)),
                      rho = c(-0.99,seq(-0.95,0.95,by=0.05), 0.99))
 grid <- rbind(grid1, grid2)
 
